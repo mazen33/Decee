807e004a2d89bc51b881c12c1d60a7809cf0fadf
mazen33 <30578968+mazen33@users.noreply.github.com>
2021-09-15 13:25:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomImage = "dice" + randomNumber1 + ".png";    //dice1.png -> dice6
+var randomImageSource ="images/" + randomImage;      //images/dice1.png -> images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomImage1 = "dice" + randomNumber2 + ".png";    //dice1.png -> dice6
+var randomImageSource1 ="images/" + randomImage1;      //images/dice1.png -> images/dice6.png
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource1);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}else if(randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
